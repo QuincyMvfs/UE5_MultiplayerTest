39edbbb37d5e18b6ee0edc5ce145369aa008b5f0
Quincy Michels <quincy.michels@gmail.com>
2024-01-30 20:37:41 -0500

    Added some code, need to make health component before countinuing

--- Source/MultiplayerTest/Components/BaseWeaponComponent.cpp
+++ Source/MultiplayerTest/Components/BaseWeaponComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "BaseWeaponComponent.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
 // Sets default values for this component's properties
 UBaseWeaponComponent::UBaseWeaponComponent()
 {
@@ -30,5 +32,51 @@ void UBaseWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+// CHECK ALL VARIABLES TO SEE IF PLAYER CAN SHOOT
+bool UBaseWeaponComponent::TryShootWeapon(UCameraComponent* cameraComponent, AActor* shooter)
+{
+	// TRUE = CAN SHOOT
+	// FALSE = CANT SHOOT
+	if (m_nextTimeToShoot <= GetWorld()->GetTimeSeconds()
+		&& m_currentMagazine > 0)
+	{
+		m_nextTimeToShoot = GetWorld()->GetTimeSeconds() + M_DelayBetweenShots;
+		m_currentMagazine--;
+		
+		ShootWeapon(cameraComponent, shooter);
+		return true;
+	}
+
+	return false;
+}
+
+// SHOOT
+void UBaseWeaponComponent::ShootWeapon(UCameraComponent* cameraComponent, AActor* shooter)
+{
+	
+}
+
+bool UBaseWeaponComponent::TryReload()
+{
+	return false;
+}
+
+void UBaseWeaponComponent::Reload()
+{
+	
+}
+
+void UBaseWeaponComponent::PerformRaycast(FVector startPoint, FVector endPoint, AActor* shooter)
+{
+	FHitResult hitResult;
+	if (bool hitObject = UKismetSystemLibrary::LineTraceSingle(
+		this, startPoint, endPoint, UEngineTypes::ConvertToTraceType(ECC_GameTraceChannel1),
+		false, TArray<AActor*>(), EDrawDebugTrace::ForDuration, hitResult,
+		true,FColor::White,FColor::Red, 0.2f))
+	{
+		
+	}
 }
 
--- Source/MultiplayerTest/Components/BaseWeaponComponent.h
+++ Source/MultiplayerTest/Components/BaseWeaponComponent.h
@@ -24,5 +24,31 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	virtual bool TryShootWeapon(UCameraComponent* cameraComponent, AActor* shooter);
+	virtual void ShootWeapon(UCameraComponent* cameraComponent, AActor* shooter);
+	virtual bool TryReload();
+	virtual void Reload();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Base Weapon Variables")
+	float M_Damage = 10.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Base Weapon Variables")
+	float M_DelayBetweenShots = 0.4f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Base Weapon Variables")
+	float M_ReloadDuration = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Base Weapon Variables")
+	int M_MaxMagazineCapacity = 30;
+
+protected:
+	virtual void PerformRaycast(FVector startPoint, FVector endPoint, AActor* shooter);
+
+	
+protected:
+	int m_currentMagazine;
+	float m_nextTimeToShoot;
+	bool m_isReloading;
+	bool m_canFire;
 };
