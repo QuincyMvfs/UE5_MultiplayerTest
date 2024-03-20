b8736e9284a6c06107c1c2d722616fe5f236833a
Quincy Michels <quincy.michels@gmail.com>
2024-03-20 12:04:07 -0400

    getting nonsense error

---
+++ Source/MultiplayerTest/EnumClasses/EGraphicSettingTypes.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class EGraphicSettingTypes:uint8
+{
+	Overall,
+	ResolutionScale,
+	ViewDistance,
+	AntiAliasing,
+	PostProcessing,
+	Shadows,
+	GlobalIllumination,
+	Reflections,
+	Textures,
+	Effects,
+	Foliage,
+	Shading,
+};
